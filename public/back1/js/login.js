28e2c3ab0cbf4acde0e02cb149b3f4a92eb7c19a
lufeng111 <1836232210@qq.com>
2018-08-24 17:29:35 +0800

    登录页面静态页完成，基本表单效验完成了

---
+++ public/back1/js/login.js
@@ -0,0 +1,51 @@
+$(function(){
+//   校验规则：
+// 1. 用户名不能为空
+// 2. 用户密码不能为空,长度为2-6位
+// 3. 用户密码  长度为6-12位
+$('#form').bootstrapValidator({
+  // 配置字段
+  fields:{
+    username:{
+      // 配置效验规则
+      validators:{
+        // 非空
+        notEmpty:{
+          // 提示信息
+          message:"用户名不能为空"
+        },
+        // 长度效验
+        stringLength:{
+          min:2,
+          max:6,
+          message:"用户名长度必须在2-6位"
+        }
+      }
+
+    },
+    password:{
+      // 配置字段
+      validators:{
+        // 非空
+        notEmpty:{
+          // 提示信息
+          message:"密码不能为空"
+        },
+        // 长度效验
+        stringLength:{
+          min:6,
+          max:12,
+          message:"密码长度必须在6-12位"
+        },
+      }
+
+    },
+  }
+});
+
+
+
+
+
+
+})
